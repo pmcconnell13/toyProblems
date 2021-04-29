7725fd7cb1285d2de852904c2f7d575d4a3595bd
Patrick McConnell <pmcconnell13@gmail.com>
2021-04-29 16:52:04 -0700

    Added insert linkedlist

--- dataStructures/linkedList.js
+++ dataStructures/linkedList.js
@@ -44,29 +44,81 @@ class LinkedList {
   }
 
   append(value) {
-    var newNode = {
-      value: value,
-      next: null
-    }
+    var newNode = new Node(value)
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++
   }
 
   prepend(value) {
-    var newNode = {
-      value: value,
-      next: null
-    }
+    var newNode = new Node(value)
     newNode.next = this.head;
     this.head = newNode;
     this.length++
   }
 
+  printList() {
+    var arr = [];
+    var currentNode = this.head;
+    while (currentNode !== null) {
+      arr.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return arr
+  }
+
+  // insert(index, value) {
+  //   var newNode = new Node(value);
+  //   var currentNode = this.head;
+  //   var count = 0;
+  //   while (currentNode !== null) {
+  //     if (count + 1 === index) {
+  //       newNode.next = currentNode.next;
+  //       currentNode.next = newNode;
+  //       return this
+  //     }
+  //       currentNode = currentNode.next
+  //       count++
+  //   }
+  // }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      this.append(value);
+    }
+    var newNode = new Node(value);
+    var leader = this.traverseList(index - 1);
+    newNode.next = leader.next;
+    leader.next = newNode;
+    this.length++
+    return this
+  }
+
+  traverseList(index) {
+    var currentNode = this.head;
+    var count = 0;
+    while (count !== index) {
+      currentNode = currentNode.next;
+      count++
+    }
+    return currentNode;
+  }
+
+}
+
+class Node {
+  constructor(value) {
+    this.value = value,
+    this.next = null
+  }
 }
 
 var myLinkedList = new LinkedList(5);
 myLinkedList.append(6);
 myLinkedList.append(7);
 myLinkedList.prepend(4);
+myLinkedList.prepend(2);
+myLinkedList.append(50);
+myLinkedList.insert(3, 12);
+console.log(myLinkedList.printList());
 console.log(myLinkedList)
