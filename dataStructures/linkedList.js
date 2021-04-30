1cb2c173e1b0752ac88e083d4e933462feedfc2d
Patrick McConnell <pmcconnell13@gmail.com>
2021-04-30 11:33:49 -0700

    added removal node

--- dataStructures/linkedList.js
+++ dataStructures/linkedList.js
@@ -33,6 +33,86 @@
 // myLinkedList.prepend(1)
 // console.log(myLinkedList)
 
+// class LinkedList {
+//   constructor(value) {
+//     this.head = {
+//       value: value,
+//       next: null
+//     }
+//     this.tail = this.head;
+//     this.length = 1
+//   }
+
+//   append(value) {
+//     var newNode = new Node(value)
+//     this.tail.next = newNode;
+//     this.tail = newNode;
+//     this.length++
+//   }
+
+//   prepend(value) {
+//     var newNode = new Node(value)
+//     newNode.next = this.head;
+//     this.head = newNode;
+//     this.length++
+//   }
+
+//   printList() {
+//     var arr = [];
+//     var currentNode = this.head;
+//     while (currentNode !== null) {
+//       arr.push(currentNode.value);
+//       currentNode = currentNode.next;
+//     }
+//     return arr
+//   }
+
+//   // insert(index, value) {
+//   //   var newNode = new Node(value);
+//   //   var currentNode = this.head;
+//   //   var count = 0;
+//   //   while (currentNode !== null) {
+//   //     if (count + 1 === index) {
+//   //       newNode.next = currentNode.next;
+//   //       currentNode.next = newNode;
+//   //       return this
+//   //     }
+//   //       currentNode = currentNode.next
+//   //       count++
+//   //   }
+//   // }
+
+//   insert(index, value) {
+//     if (index >= this.length) {
+//       this.append(value);
+//     }
+//     var newNode = new Node(value);
+//     var leader = this.traverseList(index - 1);
+//     newNode.next = leader.next;
+//     leader.next = newNode;
+//     this.length++
+//     return this
+//   }
+
+//   traverseList(index) {
+//     var currentNode = this.head;
+//     var count = 0;
+//     while (count !== index) {
+//       currentNode = currentNode.next;
+//       count++
+//     }
+//     return currentNode;
+//   }
+
+// }
+
+// class Node {
+//   constructor(value) {
+//     this.value = value,
+//     this.next = null
+//   }
+// }
+
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -44,20 +124,31 @@ class LinkedList {
   }
 
   append(value) {
-    var newNode = new Node(value)
+    var newNode = new Node(value);
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++
   }
 
   prepend(value) {
-    var newNode = new Node(value)
+    var newNode = new Node(value);
     newNode.next = this.head;
     this.head = newNode;
     this.length++
   }
 
-  printList() {
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    var newNode = new Node(value);
+    var leadNode = this.traverseToIndex(index - 1);
+    newNode.next = leadNode.next;
+    leadNode.next = newNode;
+    this.length++
+  }
+
+  printList(){
     var arr = [];
     var currentNode = this.head;
     while (currentNode !== null) {
@@ -67,36 +158,23 @@ class LinkedList {
     return arr
   }
 
-  // insert(index, value) {
-  //   var newNode = new Node(value);
-  //   var currentNode = this.head;
-  //   var count = 0;
-  //   while (currentNode !== null) {
-  //     if (count + 1 === index) {
-  //       newNode.next = currentNode.next;
-  //       currentNode.next = newNode;
-  //       return this
-  //     }
-  //       currentNode = currentNode.next
-  //       count++
-  //   }
-  // }
-
-  insert(index, value) {
+  remove(index) {
     if (index >= this.length) {
-      this.append(value);
+      return null
     }
-    var newNode = new Node(value);
-    var leader = this.traverseList(index - 1);
-    newNode.next = leader.next;
-    leader.next = newNode;
-    this.length++
-    return this
+    var leader = this.traverseToIndex(index - 1)
+    var removalNode = leader.next;
+    if (removalNode.next === null) {
+      this.tail = leader
+    }
+    leader.next = removalNode.next
+    this.length--;
+    return this;
   }
 
-  traverseList(index) {
-    var currentNode = this.head;
+  traverseToIndex(index) {
     var count = 0;
+    var currentNode = this.head;
     while (count !== index) {
       currentNode = currentNode.next;
       count++
@@ -113,12 +191,14 @@ class Node {
   }
 }
 
+
 var myLinkedList = new LinkedList(5);
 myLinkedList.append(6);
 myLinkedList.append(7);
 myLinkedList.prepend(4);
 myLinkedList.prepend(2);
 myLinkedList.append(50);
-myLinkedList.insert(3, 12);
+myLinkedList.insert(1, 12);
+myLinkedList.remove(myLinkedList.length - 1);
 console.log(myLinkedList.printList());
 console.log(myLinkedList)
