a1859bca633a505fe968820a5d3d1a05e7030d46
Patrick McConnell <pmcconnell13@gmail.com>
2021-05-03 16:30:50 -0700

    Stack

--- dataStructures/singleLinkedList.js
+++ dataStructures/singleLinkedList.js
@@ -33,85 +33,103 @@
 // myLinkedList.prepend(1)
 // console.log(myLinkedList)
 
-// class LinkedList {
-//   constructor(value) {
-//     this.head = {
-//       value: value,
-//       next: null
-//     }
-//     this.tail = this.head;
-//     this.length = 1
-//   }
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    }
+    this.tail = this.head;
+    this.length = 1
+  }
 
-//   append(value) {
-//     var newNode = new Node(value)
-//     this.tail.next = newNode;
-//     this.tail = newNode;
-//     this.length++
-//   }
+  append(value) {
+    var newNode = new Node(value)
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++
+  }
 
-//   prepend(value) {
-//     var newNode = new Node(value)
-//     newNode.next = this.head;
-//     this.head = newNode;
-//     this.length++
-//   }
+  prepend(value) {
+    var newNode = new Node(value)
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++
+  }
 
-//   printList() {
-//     var arr = [];
-//     var currentNode = this.head;
-//     while (currentNode !== null) {
-//       arr.push(currentNode.value);
-//       currentNode = currentNode.next;
-//     }
-//     return arr
-//   }
+  printList() {
+    var arr = [];
+    var currentNode = this.head;
+    while (currentNode !== null) {
+      arr.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return arr
+  }
 
-//   // insert(index, value) {
-//   //   var newNode = new Node(value);
-//   //   var currentNode = this.head;
-//   //   var count = 0;
-//   //   while (currentNode !== null) {
-//   //     if (count + 1 === index) {
-//   //       newNode.next = currentNode.next;
-//   //       currentNode.next = newNode;
-//   //       return this
-//   //     }
-//   //       currentNode = currentNode.next
-//   //       count++
-//   //   }
-//   // }
+  // insert(index, value) {
+  //   var newNode = new Node(value);
+  //   var currentNode = this.head;
+  //   var count = 0;
+  //   while (currentNode !== null) {
+  //     if (count + 1 === index) {
+  //       newNode.next = currentNode.next;
+  //       currentNode.next = newNode;
+  //       return this
+  //     }
+  //       currentNode = currentNode.next
+  //       count++
+  //   }
+  // }
 
-//   insert(index, value) {
-//     if (index >= this.length) {
-//       this.append(value);
-//     }
-//     var newNode = new Node(value);
-//     var leader = this.traverseList(index - 1);
-//     newNode.next = leader.next;
-//     leader.next = newNode;
-//     this.length++
-//     return this
-//   }
+  insert(index, value) {
+    if (index >= this.length) {
+      this.append(value);
+    }
+    var newNode = new Node(value);
+    var leader = this.traverseList(index - 1);
+    newNode.next = leader.next;
+    leader.next = newNode;
+    this.length++
+    return this
+  }
 
-//   traverseList(index) {
-//     var currentNode = this.head;
-//     var count = 0;
-//     while (count !== index) {
-//       currentNode = currentNode.next;
-//       count++
-//     }
-//     return currentNode;
-//   }
+  traverseList(index) {
+    var currentNode = this.head;
+    var count = 0;
+    while (count !== index) {
+      currentNode = currentNode.next;
+      count++
+    }
+    return currentNode;
+  }
 
-// }
+  reverse() {
+    if (!this.head.next) {
+      return this.head
+    }
+    var first = this.head;
+    this.tail = this.head;
+    var second = this.head.next;
+    while(second) {
+      var temp = second.next;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+    this.tail.next = null;
+    this.head = first;
+    return this
+  }
 
-// class Node {
-//   constructor(value) {
-//     this.value = value,
-//     this.next = null
-//   }
-// }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value,
+    this.next = null
+  }
+}
 
 
 
@@ -122,7 +140,7 @@ myLinkedList.prepend(4);
 myLinkedList.prepend(2);
 myLinkedList.append(50);
 myLinkedList.insert(1, 12);
-myLinkedList.remove(myLinkedList.length - 1);
+// myLinkedList.remove(myLinkedList.length - 1);
 console.log(myLinkedList.printList());
 console.log(myLinkedList)
 console.log(myLinkedList.reverse())
---
+++ dataStructures/stack.js
@@ -0,0 +1,53 @@
+class Node {
+  constructor(value) {
+      this.value = value,
+      this.next = null
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.top;
+  }
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    if (this.length === 1) {
+      this.bottom = null;
+    }
+    var pointer = this.top;
+    this.top = this.top.next;
+    this.length--
+    return this
+  }
+  push(value) {
+    var newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode
+    } else {
+      var pointer = this.top;
+      this.top = newNode;
+      this.top.next = pointer;
+    }
+    this.length++
+    return this
+  }
+}
+
+const myStack = new Stack();
+console.log(myStack.push(5));
+console.log(myStack.push(6));
+console.log(myStack.push(3));
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.pop());
+console.log(myStack.push(3));
+console.log(myStack.push(10));
+console.log(myStack.peek());
