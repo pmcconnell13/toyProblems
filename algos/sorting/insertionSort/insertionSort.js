82044639aede398d2821eea0be242198bb6f0e16
Patrick McConnell <pmcconnell13@gmail.com>
2021-05-10 15:38:19 -0700

    insertionSort

---
+++ algos/sorting/insertionSort/insertionSort.js
@@ -0,0 +1,16 @@
+function insertSort(array) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] < array[0]) {
+      array.unshift(array.splice(i, 1)[0])
+    } else {
+      for (var j = 1; j < i; j++) {
+        if (array[i] < array[j - 1] && array[i] < array[j]) {
+          array.splice(j, 0, array.splice(i, 1)[0])
+        }
+      }
+    }
+  }
+  return array
+}
+
+console.log(insertSort([99, 108, 45, 3, 0, 12, 94, 155, 1, 234]))
