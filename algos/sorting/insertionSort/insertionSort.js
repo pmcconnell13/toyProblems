2c0dc300ee85c1af1d458db7b0a62edb8549e1fc
Patrick McConnell <pmcconnell13@gmail.com>
2021-05-11 15:23:50 -0700

    updated insert sort

--- algos/sorting/insertionSort/insertionSort.js
+++ algos/sorting/insertionSort/insertionSort.js
@@ -4,7 +4,7 @@ function insertSort(array) {
       array.unshift(array.splice(i, 1)[0])
     } else {
       for (var j = 1; j < i; j++) {
-        if (array[i] < array[j - 1] && array[i] < array[j]) {
+         if (array[i] >= array[j - 1] && array[i] < array[j]) {
           array.splice(j, 0, array.splice(i, 1)[0])
         }
       }
