30698a1419436fa3ff907c8762ca314f46120c7e
Patrick McConnell <pmcconnell13@gmail.com>
2021-05-13 09:28:41 -0700

    factorial recursion

---
+++ algos/recursion/findFactorial.js
@@ -0,0 +1,17 @@
+function findWithRecursion(number){
+  if (number <= 1) {
+    return 1
+  }
+  return number * findWithRecursion(number - 1);
+}
+
+function findWithIteration(number){
+  var answer = 1
+  for (var i = 2; i < number; i++) {
+    answer += answer * (i + 1)
+  }
+  return answer
+}
+
+console.log(findWithRecursion(5))
+console.log(findWithIteration(5))
