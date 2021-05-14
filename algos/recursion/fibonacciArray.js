edcaf1c434c700298be1ad1625aaded98e73f93e
Patrick McConnell <pmcconnell13@gmail.com>
2021-05-14 11:48:22 -0700

    new fib

---
+++ algos/recursion/fibonacciArray.js
@@ -0,0 +1,9 @@
+// 25. Get the first n Fibonacci numbers. In the Fibonacci sequence, each subsequent
+// number is the sum of the previous two.
+// Example: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34.....
+// fibonacci(5); // [0,1,1,2,3,5]
+// Note: The 0 is not counted.
+var fibonacci = function(n) {
+
+};
+console.log(fibonacci(5));
---
+++ algos/recursion/reverseString.js
@@ -0,0 +1,10 @@
+function reverseStringRecursive (str) {
+  if (str === "") {
+    return "";
+  } else {
+    return reverseStringRecursive(str.substr(1)) + str.charAt(0);
+  }
+}
+
+console.log(reverseStringRecursive('yoyo mastery'))
+//should return: 'yretsam oyoy'
