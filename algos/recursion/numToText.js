d8d2e9202d42855aa913c05a660c3b0debd0ce38
Patrick McConnell <pmcconnell13@gmail.com>
2021-05-17 11:51:39 -0700

    numToText

---
+++ algos/recursion/numToText.js
@@ -0,0 +1,5 @@
+// 36. Given a string, return a string with digits converted to their word equivalent.
+// Assume all numbers are single digits (less than 10).
+// numToText("I have 5 dogs and 6 ponies"); // "I have five dogs and six ponies"
+var numToText = function(str) {
+};
