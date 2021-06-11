e8f0befec647b0c163fecc82b46ede75744fa51b
Patrick McConnell <pmcconnell13@gmail.com>
2021-06-11 11:43:21 -0700

    bubbleSort

--- algos/sorting/bubbleSort/bubbleSort.js
+++ algos/sorting/bubbleSort/bubbleSort.js
@@ -1,15 +1,35 @@
+// function bubbleSort(array) {
+//   for (var i = 0; i < array.length; i++) {
+//     for (var j = 0; j < array.length; j++) {
+//       if (array[j] > array[j + 1]) {
+// 				var temp = array[j];
+// 				array[j] = array[j + 1];
+// 				array[j + 1] = temp;
+// 			}
+// 		}
+// 	}
+// 	return array
+// }
+
+// // Do not edit the line below.
+// exports.bubbleSort = bubbleSort;
+
 function bubbleSort(array) {
-  for (var i = 0; i < array.length; i++) {
-    for (var j = 0; j < array.length; j++) {
-      if (array[j] > array[j + 1]) {
-				var temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-			}
-		}
+	var arrayLength = array.length;
+  //loop through array
+	for (var i = 0; i < array.length; i++) {
+		 //if current value is greater than next value
+		 for (var j = 0; j < array.length; j++) {
+			 if (array[j] >= array[j + 1]) {
+				 var temp = array[j + 1];
+				 array[j + 1] = array[j];
+				 array[j] = temp;
+			 }
+		 }
 	}
+	    //swap them
+	  //subtract 1 from array length when it gets to end
 	return array
 }
 
-// Do not edit the line below.
-exports.bubbleSort = bubbleSort;
+console.log(bubbleSort([3, 2, 6, 4, 9, 8, 8, 3]))
