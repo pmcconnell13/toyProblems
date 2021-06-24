39fafd79be289d2bea9cdd1ef6d8f5f6ea4fd07d
Patrick McConnell <pmcconnell13@gmail.com>
2021-06-24 11:22:34 -0700

    updates

--- strings/isPalindrome/isPalindrome.js
+++ strings/isPalindrome/isPalindrome.js
@@ -11,3 +11,25 @@ function isPalindrome(string) {
 // }
 
 console.log(isPalindrome('abccba'));
+
+
+//regex version
+// Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+
+
+// Example 1:
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+// Example 2:
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+
+var isPalindrome = function(s) {
+  s = s.toLowerCase().replace(/[\W_]/g,"");
+  return s.split('').reverse().join('') === s
+};
--- strings/reverseString/reverseString.js
+++ strings/reverseString/reverseString.js
@@ -27,3 +27,31 @@ function reverseWordsInString(string) {
 
 // Do not edit the line below.
 exports.reverseWordsInString = reverseWordsInString;
+
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+var reverseString = function(s) {
+	var left = 0;
+	var right = s.length - 1
+	while (left < right) {
+			var temp = s[left];
+			s[left] = s[right];
+			s[right] = temp;
+			left++;
+			right--;
+	}
+	return s
+};
+
+
