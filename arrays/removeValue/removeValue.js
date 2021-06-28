47d31a9678efebddbcf6b5c7a153ac07f567ada3
Patrick McConnell <pmcconnell13@gmail.com>
2021-06-28 10:49:42 -0700

    removeElement

---
+++ arrays/removeValue/removeValue.js
@@ -0,0 +1,11 @@
+var removeElement = function(nums, val) {
+  for (var i = 0; i < nums.length; i++) {
+      if (nums[i] === val) {
+          nums.splice(i, 1);
+          i--;
+      }
+  }
+  return nums
+};
+
+console.log(removeElement([0,1,2,2,3,0,4,2], 2))
