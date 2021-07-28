0c07eb2344226a00270529ae75742255ff668c5e
Patrick McConnell <pmcconnell13@gmail.com>
2021-07-28 10:09:55 -0700

    climbingStairs

--- Easy/climbingStairs/climbingStairs.js
+++ Easy/climbingStairs/climbingStairs.js
@@ -14,6 +14,18 @@
   return steps[n]
 };
 
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var climbStairs = function(n) {
+  let dp = [0, 1, 2];
+  for (let i = 3; i <= n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n]
+};
+
 console.log(climbStairs(7))
 
 Input: n = 2
