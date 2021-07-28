32814385bbb7ff0dd575ef269550f732812f2163
Patrick McConnell <pmcconnell13@gmail.com>
2021-07-28 11:49:50 -0700

    maxSubarrayProduct

---
+++ Medium/maxSubarrayProduct/maxSubarrayProduct.js
@@ -0,0 +1,31 @@
+var maxProduct = function(nums) {
+  let max = -Infinity;
+  let currentMax = 1;
+  let currentMin = 1;
+
+  for(let i = 0; i < nums.length; i++) {
+      let prevMax = currentMax;
+      console.log('prevMax', prevMax)
+      currentMax = Math.max(nums[i], prevMax * nums[i], currentMin * nums[i]);
+      currentMin = Math.min(nums[i], prevMax * nums[i], currentMin * nums[i]);
+      console.log('currentMax', currentMax)
+      console.log('currentMin', currentMin)
+      max = Math.max(currentMax, max);
+      console.log('max', max)
+  }
+
+  return max;
+};
+
+console.log(maxProduct([-2, 3, -4]))
+
+// Example 1:
+
+// Input: nums = [2,3,-2,4]
+// Output: 6
+// Explanation: [2,3] has the largest product 6.
+// Example 2:
+
+// Input: nums = [-2,3,-4]
+// Output: 24
+// Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
