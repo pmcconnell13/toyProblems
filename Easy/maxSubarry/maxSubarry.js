e0fb0a623ba549da3e646935806010a69a86e266
Patrick McConnell <pmcconnell13@gmail.com>
2021-07-28 11:20:18 -0700

    maxSubarray

---
+++ Easy/maxSubarry/maxSubarry.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var maxSubArray = function(nums) {
+  let maxCurrent = nums[0];
+  let maxGlobal = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    maxCurrent = Math.max(nums[i], maxCurrent + nums[i]);
+    if (maxCurrent > maxGlobal) maxGlobal = maxCurrent;
+  }
+  return maxGlobal
+};
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
