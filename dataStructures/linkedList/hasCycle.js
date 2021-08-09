4d8cfb22d71bccd7bbfe36c0c7a02e89bb8f1f6c
Patrick McConnell <pmcconnell13@gmail.com>
2021-08-09 12:04:48 -0700

    fast slow pointers

--- dataStructures/linkedList/hasCycle.js
+++ dataStructures/linkedList/hasCycle.js
@@ -19,6 +19,19 @@
 
       return false
   };
+///////////////fast slow pointers
+  var hasCycle = function(head) {
+    let fast = head;
+    let slow = head;
+
+    while (fast && fast.next) {
+      fast = fast.next.next;
+      slow = slow.next;
+
+      if (fast === slow) return true;
+    }
+    return false;
+  };
 
 
   Example 1:
